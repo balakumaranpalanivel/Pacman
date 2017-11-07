63d053e75a5c3c8be3435803af055e9175f48b71
Balakumaran Palanivel <kumaraxe@gmail.com>
2017-11-07 18:17:39 +0000

    [DEV] Implemented ReflexAgent. Getting score 2/4

--- PacMan/multiagent/multiAgents.py
+++ PacMan/multiagent/multiAgents.py
@@ -53,114 +53,61 @@ class ReflexAgent(Agent):
         "Add more of your code here if you want to"
 
         return legalMoves[chosenIndex]
-
+      
     def evaluationFunction(self, currentGameState, action):
-        """
-        Design a better evaluation function here.
+      """
+      Design a better evaluation function here.
+      The evaluation function takes in the current and proposed successor
+      GameStates (pacman.py) and returns a number, where higher numbers are better.
+      The code below extracts some useful information from the state, like the
+      remaining food (newFood) and Pacman position after moving (newPos).
+      newScaredTimes holds the number of moves that each ghost will remain
+      scared because of Pacman having eaten a power pellet.
+      Print out these variables to see what you're getting, then combine them
+      to create a masterful evaluation function.
+      """
+      # Useful information you can extract from a GameState (pacman.py)
+      successorGameState = currentGameState.generatePacmanSuccessor(action)
+      new_pacman_position = successorGameState.getPacmanPosition()
 
-        The evaluation function takes in the current and proposed successor
-        GameStates (pacman.py) and returns a number, where higher numbers are better.
+      # High score for next state if pacman is winning
+      if successorGameState.isWin():
+          return 999999
 
-        The code below extracts some useful information from t  he state, like the
-        remaining food (newFood) and Pacman position after moving (newPos).
-        newScaredTimes holds the number of moves that each ghost will remain
-        scared because of Pacman having eaten a power pellet.
+      # The state is more favourable if the ghost is more than 3 moves away from
+      # the current position of pacman
+      ghost_position = currentGameState.getGhostPosition(1)
+      ghost_distance = util.manhattanDistance(ghost_position, new_pacman_position)
+      if ghost_distance > 3:
+        score = ghost_distance * 2
+      else:
+        score = 2
 
-        Print out these variables to see what you're getting, then combine them
-        to create a masterful evaluation function.
-        """
-        # # Useful information you can extract from a GameState (pacman.py)
-        # successorGameState = currentGameState.generatePacmanSuccessor(action)
-        # # print "Action: {0}".format(action)
-        
-        # score = successorGameState.getScore()
-        # # print "Current Score: {0}".format(score)
+      # Ensuring pacman always does not go for the closest food despite nearby
+      # dangers
+      current_food = currentGameState.getFood()
+      successor_food = successorGameState.getFood()
+      closest_food = 999999
+      for food in successor_food.asList():
+        current_food = manhattanDistance(food, new_pacman_position)
+        if current_food < closest_food:
+          closest_food = current_food
+      score -= 3 * closest_food
 
-        # newPos = successorGameState.getPacmanPosition()
-        # # print "Next Position: {0}".format(newPos)
+      # But to make sure the food is getting reduced. make pacman take a
+      # move favourable to reduction of total number of food instead of 
+      # always going for the closest food
+      if (currentGameState.getNumFood() > successorGameState.getNumFood()):
+          score += 100
+      if action == Directions.STOP:
+          score -= 3
 
-        # newFood = successorGameState.getFood()
-        # # print newFood
-        # posx, posy = newPos
-        # isFood = newFood[posx+1][posy+1]
-        # # if isFood:
-        # #   print "333333333333333333333333333333333"
-        # # print "Food at Position: {0}".format(isFood)
+      # # Make pacman consuder 
+      # capsule_list = currentGameState.getCapsules()
+      # if new_pacman_position in capsule_list:
+      #     score += 150
 
-        # newGhostStates = successorGameState.getGhostStates()
-        # # print "New Ghost States: {0}".format(newGhostStates)
-
-        # newScaredTimes = [ghostState.scaredTimer for ghostState in newGhostStates]
-        # print "New Scared Times: {0}".format(newScaredTimes)
-
-        # print ""
-
-        score = 0
-        # find the number of remaining food        
-        # current_food_count = 0
-        # for item in current_food:
-        #   current_food_count += item.count(True)
-        
-        # succesor_food_count = 0
-        # for item in succesor_food:
-        #   succesor_food_count += item.count(True)
-
-        successorGameState = currentGameState.generatePacmanSuccessor(action)
-        current_food_count =  currentGameState.getNumFood()
-        succesor_food_count = successorGameState.getNumFood()
-
-        # number of food reduces or remains the same
-        # if succesor_food_count < current_food_count:
-        #   score += 1
-        # elif succesor_food_count == current_food_count:
-        #   score += 0.5
-
-        # Distance to ghost is big
-        new_ghost_states = successorGameState.getGhostStates()
-
-        new_pacman_position = successorGameState.getPacmanPosition()
-        new_position_distance = 0
-
-        current_pacman_position = currentGameState.getPacmanPosition()
-        current_position_distance = 0
-
-        for ghost_state in new_ghost_states:
-          ghost_position = ghost_state.getPosition()
-          new_position_distance += manhattanDistance(
-            ghost_position, new_pacman_position)
-          current_position_distance += manhattanDistance(
-            ghost_position, current_pacman_position)
-
-        # size of the board
-        if new_position_distance < 3:
-          if new_position_distance < current_position_distance:
-            score += -2
-          elif new_position_distance == current_position_distance:
-            score += 0.5
-          else:
-            score += 1
-
-        current_food = currentGameState.getFood()
-        successor_food = successorGameState.getFood()
-        closest_food = 1000
-
-        # find the closest food
-        for i in range(0, successor_food.width):
-          for j in range(0, len(successor_food[i])):
-            if currentGameState.hasFood(i, j):
-              current_food = manhattanDistance(new_pacman_position, (i,j))
-              if current_food < closest_food:
-                closest_food = current_food
-
-        if closest_food < (new_position_distance/2) and action is not Directions.STOP:
-          score += 5
-
-        if closest_food != 0:
-          score += 1/closest_food
-        
-        distance_to_food_list = []
-        "*** YOUR CODE HERE ***"
-        return score
+      return score
 
 def scoreEvaluationFunction(currentGameState):
     """
